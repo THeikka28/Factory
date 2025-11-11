09e4403659d2f9519434c999bcbd838557822544
Toby Heikka <tobyheikka@tobys-air-2.milton.edu>
2025-11-11 15:00:15 -0500

    Initial commit

---
+++ src/Factory.java
@@ -0,0 +1,51 @@
+public class Factory {
+    //these are instance variables, they are made in the world,
+    //they are public, and can be used anywhere
+public int yearFounded;
+public boolean isopen;
+public double earnings;
+public String products;
+//PSVM is this, it runs the code
+    public static void main(String[] args) {
+        System.out.println("Welcome to Toby's Factory");
+        new Factory();
+
+
+    }
+
+//constructor (makes a usable block of code in this case factory)
+    public Factory ()
+    {
+        products = "Houses";
+        yearFounded = 1890;
+        isopen = true;
+        earnings = 1.1;
+        System.out.println("We Make houses!");
+        factoryInfo();
+        //chnage the value of yearFounded and IsOpen
+        //then call it again
+        products = "Houses";
+        yearFounded = 2022;
+        isopen = false;
+        earnings = 0.0000000000000001;
+        factoryInfo();
+    }
+
+    public void factoryInfo()
+    {
+
+        System.out.println("Factory info:");
+        System.out.println("We were founded in " + yearFounded);
+        System.out.println("We have earned " + earnings + " million");
+        System.out.println("We sell " + products);
+        if (isopen == true) {
+            System.out.println("Come in, we're open!!!");
+        }
+        if (isopen == false) {
+            System.out.println("Get out of here, we're closed!!!");
+        }
+
+    }
+
+    //world
+}
