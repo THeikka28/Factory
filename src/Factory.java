2a06bd97b37e0413c61316d819a87230cdef6e05
THeikka28 <toby_heikka28@milton.edu>
2025-11-13 10:59:25 -0500

    We made a house class and added some text to the sout

--- src/Factory.java
+++ src/Factory.java
@@ -29,6 +29,17 @@ public String products;
         isopen = false;
         earnings = 0.0000000000000001;
         factoryInfo();
+        // ty[pe varName = value
+        //this is a variable so complex it would be called an object
+        //it is a variable made from a the class house
+        House h1 = new House();
+        h1.owner = "Gabriel";
+        h1.address = "1793 Canton Avenue, Milton ma, O2186";
+        h1.isoccupied = true;
+        h1.size = 115.23;
+        h1.walls = 5;
+        System.out.println("The house belongs to " + h1.owner + ". The address is " + h1.address + ". It is " + h1.isoccupied + " that the house is occupied" + " the house is " + h1.size+ " miles wide, and there are " + h1.walls+ " walls.");
+
     }
 
     public void factoryInfo()
---
+++ src/House.java
@@ -0,0 +1,12 @@
+public class House {
+    //instance variables
+    //1. at the top of class
+    //2. can be used anywhere but psvm
+    //3.have the world public before
+    public int walls;
+    public double size;
+    public String owner;
+    public String address;
+    public boolean isoccupied;
+
+}
